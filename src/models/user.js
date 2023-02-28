2b46eed2ce376e2281df2c180334357c84bb3a0f
Elizabeth Borokinni <elizabethborokinni@gmail.com>
2023-02-28 17:25:29 +0100

    user model

---
+++ src/models/user.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    phone_number: {
+        type: Number,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    confirm_password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        required: true,
+        enum: ["admin", "user"]
+    },
+},
+{timestamps: true}
+)
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
